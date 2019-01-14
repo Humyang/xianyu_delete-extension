7a4b764d345711ef3a86236becd85fcacc5c13ee
Yangxg <jessebutchman@gmail.com>
2019-01-14 13:15:08 +0800

    1

--- content.js
+++ content.js
@@ -5,10 +5,24 @@
 // $('a:contains(断翼折...)').remove()
 // alert('content.js')
 
+var blockList = []
 
-// setTimeout(function () {
-    chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-        $('.item-info-wrapper:contains(断翼折...)').remove()
-    })
-
-// }, 1000);
+chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
+    removeBlockList()
+})
+function removeBlockList(){
+    blockList = JSON.parse(localStorage.getItem('blockList'))||[]
+    for (let index = 0; index < blockList.length; index++) {
+        const element = blockList[index];
+        $(`.item-info-wrapper:contains(${element})`).remove()
+    }
+}
+$('.item-info-wrapper').append("<span href='#' class='delete'>delete</span>")
+$('body').on('click','.delete',function(event){  
+    let name = $(this).parents('.item-info-wrapper').find('.seller-nick a.seller-nick-name').text()
+    console.log('name',name)
+    blockList.push(name)
+    localStorage.setItem('blockList',JSON.stringify(blockList))
+    removeBlockList()
+    console.log('blockList',blockList)
+})
