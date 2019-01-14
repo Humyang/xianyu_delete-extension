197cb2e0a3a0fa7575877c7079e306e963255cbd
yangxionggui <378029633@qq.com>
2019-01-14 15:00:12 +0800

    1

--- bg.js
+++ bg.js
@@ -1,9 +1,15 @@
 chrome.browserAction.onClicked.addListener(function callback() {
-    sendMsg({id:1})
+    blockList = JSON.parse(localStorage.getItem('blockList')) || []
+    sendMsg({ blockList })
 })
 
-function sendMsg(obj){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id,obj , function(response) {});  
+function sendMsg(obj) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, obj, function (response) { });
     });
 }
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (msg) {
+        localStorage.setItem('blockList',JSON.stringify(msg.blockList))
+    });
+});
--- content.js
+++ content.js
@@ -8,21 +8,25 @@
 var blockList = []
 
 chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
+    // console.log('msg',msg)
+    blockList = msg.blockList
     removeBlockList()
 })
 function removeBlockList(){
-    blockList = JSON.parse(localStorage.getItem('blockList'))||[]
+    
     for (let index = 0; index < blockList.length; index++) {
         const element = blockList[index];
         $(`.item-info-wrapper:contains(${element})`).remove()
     }
 }
+
 $('.item-info-wrapper').append("<span href='#' class='delete'>delete</span>")
 $('body').on('click','.delete',function(event){  
     let name = $(this).parents('.item-info-wrapper').find('.seller-nick a.seller-nick-name').text()
-    console.log('name',name)
     blockList.push(name)
-    localStorage.setItem('blockList',JSON.stringify(blockList))
-    removeBlockList()
-    console.log('blockList',blockList)
+    sendMsg(blockList)
+    removeBlockList(blockList)
 })
+function sendMsg(blockList){
+    chrome.runtime.connect().postMessage({blockList});
+}
